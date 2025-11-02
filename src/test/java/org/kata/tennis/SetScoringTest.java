f34d2fa855217115cd9fc2dbeb4fbdf8d2fb4d0b
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-02 22:43:30 +0100

    🔴 Test inicialización de ganador de set como nulo

--- src/test/java/org/kata/tennis/SetScoringTest.java
+++ src/test/java/org/kata/tennis/SetScoringTest.java
@@ -2,7 +2,7 @@ package org.kata.tennis;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SetScoringTest {
 
@@ -11,5 +11,11 @@ public class SetScoringTest {
         Set set = new Set();
         assertEquals(0, set.getGames(Player.PLAYER_1));
         assertEquals(0, set.getGames(Player.PLAYER_2));
+    }
+
+    @Test
+    public void newSetShouldStartWithNoWinner() {
+        Set set = new Set();
+        assertNull(set.getWinner());
     }
 }
