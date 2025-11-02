525cf61b10a1f94c8de88eefe49b64d556b19d0e
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-02 16:06:40 +0100

    🔴 Test para comprobar que los juegos se suman correctamente

--- src/test/java/org/kata/tennis/GameScoringTest.java
+++ src/test/java/org/kata/tennis/GameScoringTest.java
@@ -1,4 +1,28 @@
 package org.kata.tennis;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameScoringTest {
+
+    @Test
+    public void on4_0ShouldAdd1ToGameScorePlayer1() {
+        // Arrange
+        Game game = new Game();
+
+        game.pointWonBy(Player.PLAYER_1);
+        game.pointWonBy(Player.PLAYER_1);
+        game.pointWonBy(Player.PLAYER_1);
+
+        int pointsBefore = game.getPointsPlayer1();
+
+        // Act
+        game.pointWonBy(Player.PLAYER_1);
+        int pointsAfter = game.getPointsPlayer1();
+        int difference = pointsAfter - pointsBefore;
+
+        // Assert
+        assertEquals(1,  difference);
+    }
 }
