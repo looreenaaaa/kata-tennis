4a6b3efb2cda8470cb98be8d866ec503eb460529
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-02 15:27:48 +0100

    🟢 Test traducción 0-0 a love-love

---
+++ src/main/java/org/kata/tennis/Score.java
@@ -0,0 +1,22 @@
+package org.kata.tennis;
+
+public class Score {
+    private int scorePlayer1;
+    private int scorePlayer2;
+
+    public Score(int scorePlayer1, int scorePlayer2) {
+        this.scorePlayer1 = scorePlayer1;
+        this.scorePlayer2 = scorePlayer2;
+    }
+
+    @Override
+    public String toString() {
+        return translateSingleScore(this.scorePlayer1) + "-" + translateSingleScore(this.scorePlayer2);
+    }
+
+    private static String translateSingleScore(int score) {
+        String result = "";
+        if (score == 0) result = "love";
+        return result;
+    }
+}
