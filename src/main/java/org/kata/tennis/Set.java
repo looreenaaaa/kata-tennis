1def97cb3bcc245c4c6891e655253a887f588102
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-02 22:46:00 +0100

    🟢 Nuevo atributo y getter de ganador en la clase Set.java

--- src/main/java/org/kata/tennis/Set.java
+++ src/main/java/org/kata/tennis/Set.java
@@ -1,10 +1,20 @@
 package org.kata.tennis;
 
 public class Set {
-    private int[] games = new int[2];
+    private int[] games;
+    private Player winner;
+
+    public Set() {
+        this.games = new int[2];
+        this.winner = null;
+    }
 
     public int getGames(Player player) {
         if (player == Player.PLAYER_1) return games[0];
         else return games[1];
+    }
+
+    public Player getWinner() {
+        return this.winner;
     }
 }
