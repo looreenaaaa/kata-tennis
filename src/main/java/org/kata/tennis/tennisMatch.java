687b8eb86501b07a2c9656f6d9f0a9522a2ed1c8
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-03 23:10:26 +0100

    tennisMatch.java y MatchWinningTest.java refactored

--- src/main/java/org/kata/tennis/tennisMatch.java
+++ src/main/java/org/kata/tennis/tennisMatch.java
@@ -1,12 +1,16 @@
 package org.kata.tennis;
 
 public class tennisMatch {
-    private int[] sets;
+    private Set set;
     private Player winner;
 
-    public tennisMatch() {
-        this.sets = new int[2];
-        this.winner = null;
+    public tennisMatch(Set set, Player winner) {
+        this.set = set;
+        this.winner = winner;
+    }
+
+    public tennisMatch(Set set) {
+        this.set = set;
     }
 
     public Player getWinner() {
@@ -17,13 +21,7 @@ public class tennisMatch {
         return winner != null;
     }
 
-    public void addSet(Player player) {
-        if (player == Player.PLAYER_1) sets[0]++;
-        else sets[1]++;
-    }
-
-    public int getSets(Player player) {
-        if (player == Player.PLAYER_1) return sets[0];
-        else return sets[1];
+    public Set getSet() {
+        return set;
     }
 }
--- src/test/java/org/kata/tennis/MatchWinningTest.java
+++ src/test/java/org/kata/tennis/MatchWinningTest.java
@@ -8,29 +8,11 @@ public class MatchWinningTest {
 
     @Test
     public void matchShouldInitializeCorrectly() {
-        tennisMatch match = new tennisMatch();
+        Set set = new Set();
+        tennisMatch match = new tennisMatch(set);
+
         assertNull(match.getWinner());
         assertFalse(match.isFinished());
-    }
-
-    @Test
-    public void correctlyAddsSetToPlayer1() {
-        tennisMatch match = new tennisMatch();
-
-        match.addSet(Player.PLAYER_1);
-
-        assertEquals(1, match.getSets(Player.PLAYER_1));
-        assertEquals(0, match.getSets(Player.PLAYER_2));
-    }
-
-    @Test
-    public void correctlyAddsSetToPlayer2() {
-        tennisMatch match = new tennisMatch();
-
-        match.addSet(Player.PLAYER_2);
-
-        assertEquals(0, match.getSets(Player.PLAYER_1));
-        assertEquals(1, match.getSets(Player.PLAYER_2));
     }
 
 }
