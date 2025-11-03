a933ca6f066ed3f56e6ea2c7295d0f6f8b89c9d8
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-03 10:51:44 +0100

    🟢 Suma correcta de sets

--- src/main/java/org/kata/tennis/tennisMatch.java
+++ src/main/java/org/kata/tennis/tennisMatch.java
@@ -1,9 +1,11 @@
 package org.kata.tennis;
 
 public class tennisMatch {
+    private int[] sets;
     private Player winner;
 
     public tennisMatch() {
+        this.sets = new int[2];
         this.winner = null;
     }
 
@@ -13,5 +15,15 @@ public class tennisMatch {
 
     public boolean isFinished() {
         return winner != null;
+    }
+
+    public void addSet(Player player) {
+        if (player == Player.PLAYER_1) sets[0]++;
+        else sets[1]++;
+    }
+
+    public int getSets(Player player) {
+        if (player == Player.PLAYER_1) return sets[0];
+        else return sets[1];
     }
 }
