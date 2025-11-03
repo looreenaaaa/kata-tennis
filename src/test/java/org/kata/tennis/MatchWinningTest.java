601258ae4afa10a8eb464f5d34612a6b0c99681a
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-03 23:21:55 +0100

    🔴 Test comprobando la lógica para ganar un partido de tenis

--- src/test/java/org/kata/tennis/MatchWinningTest.java
+++ src/test/java/org/kata/tennis/MatchWinningTest.java
@@ -15,4 +15,17 @@ public class MatchWinningTest {
         assertFalse(match.isFinished());
     }
 
+    @Test
+    public void player1WinsAt3_0Sets() {
+        Score score = new Score(3,0);
+        Game game = new Game(score, 5,4);
+        Set set = new Set(2,0, game);
+        tennisMatch match = new tennisMatch(set);
+
+        match.setWonBy(Player.PLAYER_1);
+
+        assertEquals(Player.PLAYER_1, match.getWinner());
+        assertTrue(match.isFinished());
+    }
+
 }
