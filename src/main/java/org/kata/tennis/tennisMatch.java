28429b0e2c8397a31226cc76497a7fc476bcb48c
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-03 23:31:55 +0100

    🟢 Lógica para ganar un partido de tenis implementada

--- src/main/java/org/kata/tennis/tennisMatch.java
+++ src/main/java/org/kata/tennis/tennisMatch.java
@@ -3,14 +3,27 @@ package org.kata.tennis;
 public class tennisMatch {
     private Set set;
     private Player winner;
+    private boolean matchFinished;
 
     public tennisMatch(Set set, Player winner) {
         this.set = set;
         this.winner = winner;
+        this.matchFinished = false;
     }
 
     public tennisMatch(Set set) {
         this.set = set;
+    }
+
+    public void setWonBy(Player player) {
+        set.gameWonBy(player);
+        int setPointsPlayer1 = set.getSets(Player.PLAYER_1);
+        int setPointsPlayer2 = set.getSets(Player.PLAYER_2);
+        if (setPointsPlayer1 == 3 || setPointsPlayer2 == 3) {
+            if (setPointsPlayer1 == 3) winner = Player.PLAYER_1;
+            else winner = Player.PLAYER_2;
+            matchFinished = true;
+        }
     }
 
     public Player getWinner() {
@@ -18,7 +31,7 @@ public class tennisMatch {
     }
 
     public boolean isFinished() {
-        return winner != null;
+        return matchFinished;
     }
 
     public Set getSet() {
