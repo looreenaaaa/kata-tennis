3fa511cce4bf4138ad31eb3087cc42c868dc8619
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-11-03 10:32:02 +0100

    🟢 Clase tennisMatch.java creada, junto con su constructor

---
+++ src/main/java/org/kata/tennis/tennisMatch.java
@@ -0,0 +1,13 @@
+package org.kata.tennis;
+
+public class tennisMatch {
+    private Player winner;
+
+    public tennisMatch() {
+        this.winner = null;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+}
